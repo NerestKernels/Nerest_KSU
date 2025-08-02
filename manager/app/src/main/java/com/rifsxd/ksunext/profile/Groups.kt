bf4d8b10d0bd79d5be0b659fc0d4b86568daa80b
TheFloresto <121763667+TheFloresto@users.noreply.github.com>
2025-08-03 00:13:06 +0300

    Update Groups.kt

--- manager/app/src/main/java/com/rifsxd/ksunext/profile/Groups.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/profile/Groups.kt
@@ -1,4 +1,4 @@
-package com.rifsxd.ksunext.profile
+package com.thefloresto.jksu.profile
 
 /**
  * https://cs.android.com/android/platform/superproject/main/+/main:system/core/libcutils/include/private/android_filesystem_config.h
